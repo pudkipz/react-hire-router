65667452537ce0b8db273e051c4e3da1b7098cd5
Matilda Sönnergaard <matilda.sonnergaard@se.experis.com>
2025-02-10 13:06:53 +0100

    Properly implement PersonProfile

--- src/pages/PersonProfile/index.jsx
+++ src/pages/PersonProfile/index.jsx
@@ -1,8 +1,13 @@
-import { useState } from 'react'
 import HireForm from './components/HireForm'
+import { useParams } from 'react-router-dom'
 
 function PersonProfile(props) {
-  const [person, setPerson] = useState(null)
+  // const [person, setPerson] = useState(null)
+  const people = props.people
+  const id = useParams()
+
+  const person = people.find((p) =>
+    p.id == id.id)
 
   if (!person) return <p>Loading...</p>
 
